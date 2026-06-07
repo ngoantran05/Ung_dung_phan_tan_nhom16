5ebfe3c96911aa2500e7c91ac7e4ab10055f4be4
ngoantran05 <23010154@st.phenikaa-uni.edu.vn>
2026-06-07 13:26:10 +0700

    Add files via upload

--- master.py
+++ master.py
@@ -1,33 +1,25 @@
 from kafka import KafkaConsumer
 import json
 from collections import defaultdict
-
+count = defaultdict(int)
+total = defaultdict(int)
 consumer = KafkaConsumer(
     'sensor-data',
     bootstrap_servers='localhost:9092',
-    value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+    value_deserializer=lambda m:
+        json.loads(m.decode('utf-8')),
     auto_offset_reset='earliest'
 )
-
-count = defaultdict(int)
-total = defaultdict(int)
-
+print("MASTER STARTED")
 for message in consumer:
-
     data = message.value
-
     sensor = data["sensor_id"]
     value = data["value"]
-
     count[sensor] += 1
     total[sensor] += value
-
     print("\n===== MASTER REPORT =====")
+    for key in count:
+        avg = total[key] / count[key]
+        print(f"{key}: {count[key]} messages")
+        print(f"Average Value: {avg:.2f}")
 
-    for s in count:
-
-        avg = total[s] / count[s]
-
-        print(
-            f"{s}: {count[s]} messages | Average = {avg:.2f}"
-        )
